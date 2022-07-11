3292734bcb44ef97e57cf0efcb4d92018972915f
muhammetakkurt <m_akkurt@live.com>
2022-07-12 02:25:04 +0300

    unit tests.

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,4 +1,14 @@
 <?php
 
+use Illuminate\Support\Facades\Validator;
+
 uses(Tests\TestCase::class)->in('Feature');
 uses(Tests\TestCase::class)->in('Unit');
+
+function createRequest($class, $requestBody){
+    $createdRequest = new $class;
+    $createdRequest->setContainer(app());
+    $createdRequest->initialize($requestBody);
+    $createdRequest->setValidator(Validator::make($createdRequest->all(), $createdRequest->rules()));
+    return $createdRequest;
+}
