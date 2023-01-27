a9fc581252d79f688e328deb642811d961eb26d8
Muhammet AKKURT <m_akkurt@live.com>
2023-01-27 23:47:08 +0300

    php cs fixer configuration.

--- app/Http/Controllers/API/V1/Auth/AuthController.php
+++ app/Http/Controllers/API/V1/Auth/AuthController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers\API\V1\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Services\Auth\AuthService;
+use Illuminate\Http\JsonResponse;
 
 class AuthController extends Controller
 {
-    public function postLogin(LoginRequest $request, AuthService $authService){
+    public function postLogin(LoginRequest $request, AuthService $authService): JsonResponse
+    {
         return response()->json([
             'success' => true,
-            'data' => $authService->login($request)
+            'data' => $authService->login($request),
         ]);
     }
 }
--- app/Services/Auth/AuthService.php
+++ app/Services/Auth/AuthService.php
@@ -7,17 +7,20 @@ use App\Models\User;
 use Exception;
 use Illuminate\Support\Facades\Hash;
 
-class AuthService {
-
-    public function login(LoginRequest $request){
-        
+class AuthService
+{
+    /**
+     * @throws Exception
+     */
+    public function login(LoginRequest $request): array
+    {
         $validated = $request->validated();
         $user = User::whereEmail($validated['email'])->first();
 
         if (empty($user) || ! Hash::check($validated['password'], $user->password)) {
             throw new Exception('Invalid credentials');
         }
-        
+
         return [
             'token' => $user->createToken('api_token')->plainTextToken,
             'token_type'   => 'Bearer',
